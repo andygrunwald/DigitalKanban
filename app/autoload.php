b8912c070780b706c3c4d2c96f6d4d918d674cf7
Markus Poerschke <markus@eluceo.de>
2013-05-28 23:10:56 +0200

    Fixed deps
    The symfony/DoctrineFixturesBundle has been moved.

--- app/autoload.php
+++ app/autoload.php
@@ -12,6 +12,7 @@ $loader->registerNamespaces(array(
     'Doctrine\\Common'                 => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\\DBAL'                   => __DIR__.'/../vendor/doctrine-dbal/lib',
     'Doctrine\\DBAL\\Migrations'       => __DIR__.'/../vendor/doctrine-migrations/lib',
+    'Doctrine\\Bundle'                 => __DIR__.'/../vendor/bundles',
     'Doctrine'                         => __DIR__.'/../vendor/doctrine/lib',
     'Monolog'                          => __DIR__.'/../vendor/monolog/src',
     'Assetic'                          => __DIR__.'/../vendor/assetic/src',
