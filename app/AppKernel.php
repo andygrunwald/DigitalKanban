aca408457a772b071326b708ca38e9b67443f762
Markus Poerschke <markus@eluceo.de>
2013-05-29 20:21:59 +0200

    Updated to Symfony 2.2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,13 +12,13 @@ class AppKernel extends Kernel
 			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
 			new Symfony\Bundle\TwigBundle\TwigBundle(),
 			new Symfony\Bundle\MonologBundle\MonologBundle(),
-			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-			new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-			new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
+			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+			new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
 			new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-		    new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
+		    new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 			new DigitalKanban\BaseBundle\DigitalKanbanBaseBundle(),
 		);
 
